097ecfe2793b14d4044ae7e5f38069b3d9870ee1
apoorv munshi <apoorvmunshi@gmail.com>
2016-08-31 13:57:40 -0400

    create file

---
+++ Set-1/cryptopals_S1C3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2.7
+
+#Matasano Cryptopals Challenge 3 - Set 1
+#author - Apoorv Munshi
+
+
+
+print "Matasano Cryptopals Challenge 3 from Set 1\n\n\n"
+print "Challenge description:  https://cryptopals.com/sets/1/challenges/3\n\n\n"
