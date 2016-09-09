d57fa9bda4607f57dcd4e65a6669484a52863926
Apoorv Munshi <apoorvmunshi@gmail.com>
2016-09-09 19:49:49 -0400

    8 bit bin-to-ascii dictionary

--- Set-1/cryptopals_S1C3.py
+++ Set-1/cryptopals_S1C3.py
@@ -39,14 +39,25 @@ ciphertext = raw_input("Please enter the hex encoded ciphertext:").upper()
 bin_ciphertext = ''
 bin_ciphertext = bin_ciphertext + Conv_Hex_To_Bin(ciphertext)
 
-#print bin_ciphertext
-#print len(bin_ciphertext)
+print "Ciphertext in binary is\n\n" + bin_ciphertext + "\n\n"
+print "Length is \n\n" + str(len(bin_ciphertext))
 
 #Using each ascii character as a key, XOR the key
 #with ciphertext and store results in a dictionary
 
+#Dictionary of binary-to-ascii mapping for ASCII characters in the extended ASCII set
 key_dict = {}
-for i in range (33,126,1):
-    key_dict.update({ chr(i) : bin(int(binascii.hexlify(chr(i)), 16))})
+#33-126
+for i in range (32,127,1):
+    #key_dict.update({ chr(i) : bin(int(binascii.hexlify(chr(i)), 16))})
+    key_dict.update({ (str(bin(int(binascii.hexlify(chr(i)), 16))).replace("0b","",1)).zfill(8) : chr(i) })
 
-print key_dict
+print "\n\n\n The dictionary is : \n\n\n\n%r" %key_dict
+
+for k in range(len(key_dict)):
+    key = ''
+    for i in range(len(bin_ciphertext)/8):
+        key = key + key_dict[key_dict.keys()[3]]
+
+print "\n\n\n\nkey is " + key + "\n\n\n\n"
+print "Length of the key is " + len(key)
